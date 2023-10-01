d87a4f8509d95bf19837be344674d0400c7c82f8
Viktoriia Ostrishko <vik.ostrishko@gmail.com>
2023-10-01 21:36:37 +0300

    delete old folder

--- src/components/gameHistory/board/sqare/Square.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "./square.module.css";
-
-export default function Square({value, onSquareClick}) {
-
-  return (
-    <button onClick={onSquareClick} className={styles.square}>{value}</button>
-  )
-}
